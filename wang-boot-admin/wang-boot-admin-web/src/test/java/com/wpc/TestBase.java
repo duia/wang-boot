46cb536003df4e2cce0079747818a430f23994da
王鹏程 <wangpengcheng@duia.com>
2019-05-21 15:09:57 +0800

    添加sentienl功能，实现限流功能

---
+++ wang-boot-admin/wang-boot-admin-web/src/test/java/com/wpc/TestBase.java
@@ -0,0 +1,13 @@
+package com.wpc;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by 王鹏程 on 2018/11/19 0019.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {WangBootAdminWebApplication.class})
+public class TestBase {
+}
