15605f16feee9f19f10a9a1f7e084c871baf5d6d
王鹏程 <wangpengcheng@duia.com>
2019-05-23 14:28:51 +0800

    rabbitMQ功能添加 demo

--- wang-boot-admin/wang-boot-admin-web/src/main/java/com/wpc/test/controller/DemoController.java
+++ wang-boot-admin/wang-boot-admin-web/src/main/java/com/wpc/test/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.wpc.test.controller;
 
 import com.github.pagehelper.PageInfo;
+import com.rabbitmq.client.Channel;
 import com.wpc.base.entity.datatables.DataTablesRequest;
 import com.wpc.base.entity.datatables.DataTablesResponse;
 import com.wpc.common.bean.ResponseResult;
@@ -10,22 +11,42 @@ import com.wpc.system.model.User;
 import com.wpc.system.service.IUserService;
 import com.wpc.test.dao.DemoDao;
 import com.wpc.test.entity.Demo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.locks.ReentrantLock;
 
 @Controller
 @RequestMapping("/test")
 public class DemoController {
 
+    private Logger logger = LoggerFactory.getLogger(DemoController.class);
+
     @Autowired
     private DemoDao demoDao;
     @Autowired
     private IUserService userService;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @Autowired
     private RedisDistributedLockTemplate lockTemplate;
@@ -100,6 +121,40 @@ public class DemoController {
                 lock.unlock();
             }
         }
+    }
+
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+    @GetMapping("send")
+    @ResponseBody
+    public ResponseResult send() throws Exception {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("number", "12345");
+        properties.put("send_time", simpleDateFormat.format(new Date()));
+        MessageHeaders mhs = new MessageHeaders(properties);
+        Message<Object> msg = MessageBuilder.createMessage("Hello RabbitMQ For Spring Boot!", mhs);
+//        "certificate-exam-direct", "certificate-exam-result-key"
+        rabbitTemplate.convertAndSend("a-test-exchange", "a-test-key", msg);
+        return ResponseResult.success();
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "a-test-queue",
+                    durable="true"),
+            exchange = @Exchange(value = "a-test-exchange",
+                    durable="true",
+                    type= "direct",
+                    ignoreDeclarationExceptions = "true"),
+            key = "a-test-key"
+        )
+    )
+    @RabbitHandler
+    public void onMessage(Message message, Channel channel) throws Exception {
+        logger.info("--------------------------------------");
+        logger.info("消费端Payload: " + message.getPayload());
+        Long deliveryTag = (Long)message.getHeaders().get(AmqpHeaders.DELIVERY_TAG);
+        //手工ACK
+        channel.basicAck(deliveryTag, false);
     }
 
     public static void main(String[] args) {
---
+++ wang-boot-common/src/main/java/com/wpc/common/config/RabbitmqConfig.java
@@ -0,0 +1,29 @@
+package com.wpc.common.config;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(RabbitmqConfig.class);
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(CachingConnectionFactory connectionFactory){
+        connectionFactory.setPublisherConfirms(true);
+        connectionFactory.setPublisherReturns(true);
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> log.info("消息发送成功:correlationData({}),ack({}),cause({})",correlationData,ack,cause));
+        rabbitTemplate.setReturnCallback((message, replyCode, replyText, exchange, routingKey) -> log.info("消息丢失:exchange({}),route({}),replyCode({}),replyText({}),message:{}",exchange,routingKey,replyCode,replyText,message));
+        return rabbitTemplate;
+    }
+}
